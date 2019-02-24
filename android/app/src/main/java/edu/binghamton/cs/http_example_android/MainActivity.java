c761a07c406d6af5f4488f66c172e280365c6ade
Patrick Madden <pmadden@Nostromo-2.local>
2019-02-23 19:50:16 -0500

    Built the Android version of the tool.  Looks like things should work with both iOS and Android

--- android/app/src/main/java/edu/binghamton/cs/http_example_android/MainActivity.java
+++ android/app/src/main/java/edu/binghamton/cs/http_example_android/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.binghamton.cs.http_example_android;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,36 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+interface NetResponse {
+    void netResult(Integer code, JSONArray json);
+}
+
+
+public class MainActivity extends AppCompatActivity implements NetResponse {
+    NetTask netTask;
+    EditText inputText;
+    Button computeButton;
+    MainActivity handle;
+    TextView computeResult;
+    String updateString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +47,59 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // netTask = new NetTask("https://cs.binghamton.edu/~pmadden/php/double.php", this);
+
+        inputText = findViewById(R.id.inputvalue);
+        computeButton = findViewById(R.id.compute);
+        computeResult = findViewById(R.id.computeresult);
+        handle = this;
+
+        computeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                System.out.println("Clicked the compute button");
+                String v = inputText.getText().toString();
+                System.out.println(v);
+                String request = "value=" + v;
+                netTask = new NetTask("https://cs.binghamton.edu/~pmadden/php/double.php", request, handle);
+
+                netTask.execute((Void) null);
+
             }
         });
+    }
+
+    public void netResult(Integer code, JSONArray json)
+    {
+        System.out.println("Got a result from the web");
+        updateString = "";
+
+        for (int i = 0; i < json.length(); ++i)
+        {
+            try {
+                JSONObject item = json.getJSONObject(i);
+
+                if (item.getString("result") != null) {
+                    System.out.println("Found a match");
+                    System.out.println(item.getString("result"));
+                    updateString = item.getString("result");
+
+
+                }
+            }
+            catch (JSONException e)
+            {
+                updateString = "JSON Error!";
+            }
+
+            this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    computeResult.setText(updateString);
+
+                }
+            });
+        }
     }
 
     @Override
@@ -49,4 +123,93 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    public class NetTask extends AsyncTask<Void, Void, Boolean> {
+        private final String urlString;
+        private final String reqString;
+        private NetResponse changeListener;
+
+        NetTask(String url, String request, NetResponse responseListener) {
+            urlString = url; reqString = request; changeListener = responseListener;
+        }
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                System.out.println("JSON Query: " + reqString);
+                // JSONObject json = readJsonFromUrl("https://graph.facebook.com/19292868552");
+                // JSONObject json = readJsonFromUrl("https://cnn.com");
+                // System.out.println(reqString);
+                JSONArray json = readJsonFromUrl(reqString);
+                System.out.println("Finished getting json.");
+                if (json != null)
+                    System.out.println(json.toString());
+
+                if (changeListener != null)
+                    changeListener.netResult(0, json);
+
+                //System.out.println("Notify that JSON has come in");
+                // if (noteConnector != null)
+                //    noteConnector.ncnotify(0, "");
+
+            } catch (IOException e) {
+                System.out.println("IO exception");
+                //System.out.println(e);
+                if (changeListener != null)
+                    changeListener.netResult(1, null);
+            } catch (JSONException e) {
+                System.out.println("JSON Didn't work");
+                //System.out.println(e);
+                if (changeListener != null)
+                    changeListener.netResult(2, null);
+            }
+            return true;
+        }
+
+        private String readAll(Reader rd) throws IOException {
+            StringBuilder sb = new StringBuilder();
+            int cp;
+            while ((cp = rd.read()) != -1) {
+                sb.append((char) cp);
+            }
+            System.out.println("Read from the URL");
+            System.out.println(sb.toString());
+            System.out.println("Going to try to turn it into json");
+            return sb.toString();
+        }
+
+        public JSONArray readJsonFromUrl(String request) throws IOException, JSONException {
+            URL nurl = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) nurl.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            System.out.println("Network request to " + urlString + " with request " + reqString);
+            OutputStream urlout = connection.getOutputStream();
+
+            //String s = "id=3452&second=fjfjfjfj";
+            urlout.write(request.getBytes());
+            urlout.close();
+            InputStream is = connection.getInputStream();
+
+            System.out.println("Waiting for network stream");
+            try {
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                String jsonText = readAll(rd);
+                System.out.println("JSON is " + jsonText);
+
+                JSONArray jarray = new JSONArray(jsonText);
+
+
+                System.out.println("Got the object");
+                return jarray;
+            } finally {
+                is.close();
+                // System.out.println("Did not get the object.");
+            }
+
+
+        }
+    }
+
 }
