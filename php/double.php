7aaf365e79b72ed4cc5031221b8431b01e8771fb
Patrick Madden <pmadden@Nostromo-2.local>
2019-02-24 15:30:07 -0500

    Cleaned up the PHP script a bit.

--- php/double.php
+++ php/double.php
@@ -9,11 +9,9 @@
 $x = $_POST["value"];
 $x = 2 * $x;
 
-$result = array();
 $result[] = array('result' => $x, 'status' => 0, 'input' => $_POST["value"], 'string' => "a string");
 
 $js = json_encode($result);
 
 echo $js;
-
 ?>
